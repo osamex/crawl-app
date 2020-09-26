e68358732b4f77c6ae5e10b1592af120559c9656
ooleszkiewicz <oskar.oleszkiewicz@gmail.com>
2020-09-26 12:17:02 +0200

    Initial commit

---
+++ crawl.webapi/Crawl.WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Crawl.WebAPI.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	[Authorize]
+	public class AuthController : Controller
+	{
+		[HttpGet("IsAlive")]
+		[AllowAnonymous]
+		public IActionResult IsAlive()
+		{
+			try
+			{
+				return Ok("Is alive!");
+			}
+			catch (Exception e)
+			{
+				return BadRequest(e.Message);
+			}
+		}
+	}
+}
